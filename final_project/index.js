dbe9051ce2da0401ad9accb077ee604dd192aac5
Indhira1989 <indharjo@gmail.com>
2024-08-22 23:26:42 -0400

    task 1 commit

--- final_project/index.js
+++ final_project/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const books = require('./router/booksdb.js');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
@@ -11,12 +12,27 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Check if the user has an access token in the session
+  if (!req.session.token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  // Verify the token
+  jwt.verify(req.session.token, 'your_jwt_secret', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    // Token is valid, proceed to the next middleware or route
+    req.user = decoded; // Attach user info to the request object
+    next();
+  });
 });
  
-const PORT =5000;
+
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
+const PORT =5000;
 app.listen(PORT,()=>console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,9 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  console.log("Fetching all books...");  
+  res.json(books); // Assumes books is an array of book objects
 });
 
 // Get book details based on ISBN
